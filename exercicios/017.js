08ffa1661b7fbbe7a6be9efde1b5e5c521793db7
maria clara 💛 <maria.clara112811@gmail.com>
2025-09-23 12:17:35 -0300

    revisao js

--- exercicios/017.js
+++ exercicios/017.js
@@ -1,20 +1,16 @@
 const soma = (a, b) => {
     console.log(a + b);
 }
-
 const subtracao = (a, b) => {
     console.log(a - b);
 }
-
 const multiplicacao = (a, b) => {
     console.log(a * b);
 }
-
 const divisao = (a, b) => {
     console.log(a / b);
 }
 
-// Chamadas
 soma(10, 5);
 subtracao(10, 5);
 multiplicacao(10, 5);
